a72e212145c77ccfe6cd8a05c0a2bd8201e7bec9
Nagendrapanga <nagendrapanga1069@gmail.com>
2023-01-29 10:14:33 +0530

    parkplus

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -16,6 +18,32 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+
+        Payment payment = new Payment();
+
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+
+        int cost = reservation.getNumberOfHours()*reservation.getSpot().getPricePerHour();
+
+        if(cost>amountSent)
+            throw new Exception("Insufficient Amount");
+        if(mode.toLowerCase().equals("cash")) {
+            payment.setPaymentMode(PaymentMode.CASH);
+
+        } else if(mode.toLowerCase().equals("card")) {
+            payment.setPaymentMode(PaymentMode.CARD);
+        } else if(mode.toLowerCase().equals("upi")) {
+            payment.setPaymentMode(PaymentMode.UPI);
+        }
+        else
+            throw new Exception("Payment mode not detected");
+
+        payment.setReservation(reservation);
+        reservation.setPayment(payment);
+        payment.setPaymentCompleted(true);
+        paymentRepository2.save(payment);
+
+        return payment;
 
     }
 }
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -24,5 +24,47 @@ public class ReservationServiceImpl implements ReservationService {
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
 
+
+            if(!userRepository3.existsById(userId))
+                throw new Exception("Cannot make reservation");
+            if(!parkingLotRepository3.existsById(parkingLotId))
+                throw new Exception("Cannot make reservation");
+
+            User user = userRepository3.findById(userId).get();
+
+            ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+
+
+            List<Spot> spots = parkingLot.getSpotList();
+            Spot ospot = null;
+            int price = Integer.MAX_VALUE;
+            for (Spot sp : spots) {
+                int wheels;
+                if (sp.getSpotType() == SpotType.TWO_WHEELER) {
+                    wheels = 2;
+                } else if (sp.getSpotType() == SpotType.FOUR_WHEELER) {
+                    wheels = 4;
+                } else {
+                    wheels = 100;
+                }
+                if (!sp.getOccupied() && wheels > numberOfWheels && sp.getPricePerHour()< price) {
+                    ospot = sp;
+                    price = sp.getPricePerHour();
+                }
+            }
+            if(ospot==null)
+                throw new Exception("Cannot make reservation");
+
+            Reservation reservation = new Reservation();
+            reservation.setNumberOfHours(timeInHours);
+            reservation.setUser(user);
+            reservation.setSpot(ospot);
+            ospot.setOccupied(true);
+            ospot.getReservationList().add(reservation);
+            user.getReservationList().add(reservation);
+            reservationRepository3.save(reservation);
+            return reservation;
+
     }
+
 }
